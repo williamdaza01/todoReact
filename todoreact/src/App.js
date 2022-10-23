afa4dfa0309a582dab471f38b0f32376d3acf042
Daza <williamdaza04@gmail.com>
2022-10-23 08:59:23 -0500

    Se agregan funcion de agregar

--- todoreact/src/App.js
+++ todoreact/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import data from './data.json';
 import { useState } from 'react';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
 
@@ -13,9 +14,19 @@ function App() {
     }));
   }
 
+  const onDeleteItem = (id) => {
+    setTodo([...todos].filter(todo => todo.id !== id));
+  } 
+
+  const addTodo = (newTodo) => {
+    let newItem = {id : +new Date(), task : newTodo, completed : false};
+    setTodo([...todos, newItem])
+  }
+
   return (
     <div className="container">
-      <TodoList todos = {todos} onComplete={onComplete}/>
+      <TodoForm addTodo = {addTodo}/>
+      <TodoList todos = {todos} onComplete={onComplete} onDeleteItem ={onDeleteItem}/>
     </div>
   );
 }
